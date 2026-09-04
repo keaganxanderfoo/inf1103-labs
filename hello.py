7650cb3b5d6e485fb48226cc037aad15c5b74c69
Keagan Foo <2605804@sit.singaporetech.edu.sg>
2026-09-04 10:50:44 +0800

    Update Profile

--- hello.py
+++ hello.py
@@ -2,3 +2,20 @@ print("=================================")
 print("Welcome here")
 print("My first post!")
 print("=================================")
+
+username = "abitkeagan"
+bio = "God will provide"
+followers = 1195
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers += 10
+print("Day 3:", followers)
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
