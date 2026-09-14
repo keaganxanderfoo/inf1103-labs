7caf430876bf67ede966c6f3544cd35c3d8f8390
Keagan Foo <2605804@sit.singaporetech.edu.sg>
2026-09-14 22:45:06 +0800

    String Error Check

--- auditor.py
+++ auditor.py
@@ -3,6 +3,12 @@ stock_quantity = 0
 
 while stock_quantity != "quit":
     stock_quantity = input("Enter stock quantity: ")
-    if stock_quantity != "quit":
+    if stock_quantity.isdigit():
         inventory += int(stock_quantity)
+        print(inventory)
+        
+    
+        
+        
+        
         
