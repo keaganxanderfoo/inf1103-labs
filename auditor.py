71cf2f350415e08dc7d5a0b0ff08f5cf4ddb5fbd
Keagan Foo <2605804@sit.singaporetech.edu.sg>
2026-09-14 23:23:12 +0800

    Invalid input and Processed Units

--- auditor.py
+++ auditor.py
@@ -1,20 +1,27 @@
 inventory = 0
 stock_quantity = 0
+invalid_input = 0
+processed_units = 0
 
 while stock_quantity != "quit":
     stock_quantity = input("Enter stock quantity: ")
 
-    if stock_quantity == "quit":
+    if stock_quantity == "quit": 
          break
 
     if stock_quantity.isdigit(): #Negative numbers are not digits, hence serves double error handling
         inventory += int(stock_quantity)
+        processed_units += int(stock_quantity)
         print(inventory)
         if inventory > 500:
             print("Inventory has exceeded 500 units.")
             break
     else:
         print("Invalid input, please enter only integers")    
+        invalid_input += 1
+
+print("Total Processed Units:", processed_units)
+print("Total Invalid Inputs:", invalid_input)
 
         
        
