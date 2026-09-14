b6aa13f6ca2cd12f0fe59d67628ab0ad62d69732
Keagan Foo <2605804@sit.singaporetech.edu.sg>
2026-09-14 23:09:40 +0800

    Negative numbers and String error handling

--- auditor.py
+++ auditor.py
@@ -3,9 +3,21 @@ stock_quantity = 0
 
 while stock_quantity != "quit":
     stock_quantity = input("Enter stock quantity: ")
-    if stock_quantity.isdigit():
+
+    if stock_quantity == "quit":
+         break
+
+    if stock_quantity.isdigit(): #Negative numbers are not digits, hence serves double error handling
         inventory += int(stock_quantity)
         print(inventory)
+        if inventory > 500:
+            print("Inventory has exceeded 500 units.")
+            break
+    else:
+        print("Invalid input, please enter only integers")    
+
+        
+       
         
     
         
