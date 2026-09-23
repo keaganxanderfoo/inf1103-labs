f5adb8a66f66495ff275878490fb0a7ad2a4f486
Keagan Foo <2605804@sit.singaporetech.edu.sg>
2026-09-23 14:14:49 +0800

    Inventory limit handler and Report Generator

--- Lab 3/modular_auditor.py
+++ Lab 3/modular_auditor.py
@@ -12,7 +12,11 @@ def process_delivery(current_total, new_value):
     return current_total + new_value
 
 def calculate_tax(amount):
-    return amount * 0.1
+    return round(amount * 0.1)
+
+def generate_report(total_units, failed_attempts):
+    print("Total Processed Units:", total_units)
+    print("Total Invalid Inputs:", failed_attempts)
 
 inventory = 0
 invalid_input = 0
@@ -27,7 +31,14 @@ while True:  #Create infinite loop
         invalid_input += 1
 
     inventory = process_delivery(stock_quantity, inventory)
+
+    if inventory > 500:
+        print("Inventory has been overstocked")
+        break
+
     tax = calculate_tax(inventory)
+
+
 print(inventory, invalid_input, tax)
 
 
